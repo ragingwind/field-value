1f0d225d9fd19410e4ac749eadb731af602d7c8d
ragingwind <ragingwind@gmail.com>
2016-07-11 09:00:00 +0900

    Stop using babel

--- index.js
+++ index.js
@@ -1,110 +1,39 @@
 'use strict';
 
-var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; })();
-
-function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj; }
-
 function valuefy(value) {
-	if ((typeof value === 'undefined' ? 'undefined' : _typeof(value)) !== 'object') {
-		return typeof value === 'string' ? '"' + value + '"' : value;
+	if (typeof value !== 'object') {
+		return (typeof value === 'string') ? `"${value}"` : value;
 	}
 
-	var values = [];
+	let values = [];
 
 	if (Array.isArray(value)) {
-		var _iteratorNormalCompletion = true;
-		var _didIteratorError = false;
-		var _iteratorError = undefined;
-
-		try {
-			for (var _iterator = value[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-				var v = _step.value;
-
-				values.push(valuefy(v));
-			}
-		} catch (err) {
-			_didIteratorError = true;
-			_iteratorError = err;
-		} finally {
-			try {
-				if (!_iteratorNormalCompletion && _iterator.return) {
-					_iterator.return();
-				}
-			} finally {
-				if (_didIteratorError) {
-					throw _iteratorError;
-				}
-			}
+		for (const v of value) {
+			values.push(valuefy(v));
 		}
-
-		values = '[' + values.join(',') + ']';
+		values = `[${values.join(',')}]`;
 	} else {
-		var _iteratorNormalCompletion2 = true;
-		var _didIteratorError2 = false;
-		var _iteratorError2 = undefined;
-
-		try {
-			for (var _iterator2 = Object.entries(value)[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-				var _step2$value = _slicedToArray(_step2.value, 2);
-
-				var key = _step2$value[0];
-				var v = _step2$value[1];
-
-				values.push('"' + key + '":' + valuefy(v));
-			}
-		} catch (err) {
-			_didIteratorError2 = true;
-			_iteratorError2 = err;
-		} finally {
-			try {
-				if (!_iteratorNormalCompletion2 && _iterator2.return) {
-					_iterator2.return();
-				}
-			} finally {
-				if (_didIteratorError2) {
-					throw _iteratorError2;
-				}
+		for (let v in value) {
+			if ({}.hasOwnProperty.call(value, v)) {
+				values.push(`"${v}":${valuefy(value[v])}`);
 			}
 		}
-
-		values = '{' + values.join(',') + '}';
+		values = `{${values.join(',')}}`;
 	}
 
 	return values;
 }
 
 function serialize(target) {
-	if (!target || (typeof target === 'undefined' ? 'undefined' : _typeof(target)) !== 'object') {
+	if (!target || typeof target !== 'object') {
 		throw new TypeError('Invalid type of target');
 	}
 
 	var values = [];
 
-	var _iteratorNormalCompletion3 = true;
-	var _didIteratorError3 = false;
-	var _iteratorError3 = undefined;
-
-	try {
-		for (var _iterator3 = Object.entries(target)[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-			var _step3$value = _slicedToArray(_step3.value, 2);
-
-			var key = _step3$value[0];
-			var value = _step3$value[1];
-
-			values.push(key + '=' + valuefy(value));
-		}
-	} catch (err) {
-		_didIteratorError3 = true;
-		_iteratorError3 = err;
-	} finally {
-		try {
-			if (!_iteratorNormalCompletion3 && _iterator3.return) {
-				_iterator3.return();
-			}
-		} finally {
-			if (_didIteratorError3) {
-				throw _iteratorError3;
-			}
+	for (let t in target) {
+		if ({}.hasOwnProperty.call(target, t)) {
+			values.push(`${t}=${valuefy(target[t])}`);
 		}
 	}
 
@@ -112,8 +41,8 @@ function serialize(target) {
 }
 
 function extract(t, outter, onlyContent) {
-	var start = onlyContent ? 1 : 0;
-	var pad = onlyContent ? 0 : 1;
+	const start = onlyContent ? 1 : 0;
+	const pad = onlyContent ? 0 : 1;
 	return t.slice(start, t.lastIndexOf(outter) + pad);
 }
 
@@ -121,35 +50,14 @@ function objectify(v) {
 	if (v[0] === '{') {
 		return JSON.parse(extract(v, '}'));
 	} else if (v[0] === '[') {
-		var set = [];
-		var es = extract(v, ']', true);
+		let set = [];
+		let es = extract(v, ']', true);
 
 		if (es[0] === '[' || es[0] === '{') {
 			set.push(objectify(es));
 		} else {
-			var _iteratorNormalCompletion4 = true;
-			var _didIteratorError4 = false;
-			var _iteratorError4 = undefined;
-
-			try {
-				for (var _iterator4 = es.split(',')[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
-					var e = _step4.value;
-
-					set.push(objectify(e));
-				}
-			} catch (err) {
-				_didIteratorError4 = true;
-				_iteratorError4 = err;
-			} finally {
-				try {
-					if (!_iteratorNormalCompletion4 && _iterator4.return) {
-						_iterator4.return();
-					}
-				} finally {
-					if (_didIteratorError4) {
-						throw _iteratorError4;
-					}
-				}
+			for (const e of es.split(',')) {
+				set.push(objectify(e));
 			}
 		}
 
@@ -168,32 +76,11 @@ function deserialize(values) {
 		values = [values];
 	}
 
-	var target = {};
+	let target = {};
 
-	var _iteratorNormalCompletion5 = true;
-	var _didIteratorError5 = false;
-	var _iteratorError5 = undefined;
-
-	try {
-		for (var _iterator5 = values[Symbol.iterator](), _step5; !(_iteratorNormalCompletion5 = (_step5 = _iterator5.next()).done); _iteratorNormalCompletion5 = true) {
-			var v = _step5.value;
-
-			var fieldValue = v.split('=', 2);
-			target[fieldValue[0]] = objectify(fieldValue[1]);
-		}
-	} catch (err) {
-		_didIteratorError5 = true;
-		_iteratorError5 = err;
-	} finally {
-		try {
-			if (!_iteratorNormalCompletion5 && _iterator5.return) {
-				_iterator5.return();
-			}
-		} finally {
-			if (_didIteratorError5) {
-				throw _iteratorError5;
-			}
-		}
+	for (const v of values) {
+		const fieldValue = v.split('=', 2);
+		target[fieldValue[0]] = objectify(fieldValue[1]);
 	}
 
 	return target;
--- test.js
+++ test.js
@@ -1,55 +1,53 @@
 import test from 'ava';
-import fn from './';
 import deep from 'deep-equal';
+import fn from './';
 
 const target = {
-	'key1': 'value1',
-	'key2': 1,
-	'key3': [3, 'value3'],
-	'key4': {
-		'key4_1': 'value4_1',
-		'key4_2': 'value4_2'
+	key1: 'value1',
+	key2: 1,
+	key3: [3, 'value3'],
+	key4: {
+		key41: 'value41',
+		key42: 'value42'
 	},
-	'key5': [
-		'value5_1', 'value5_1', 'value5_1'
+	key5: [
+		'value51', 'value51', 'value51'
 	],
-	'key6': {
-		'key6_1': {
-			'key6_1_1': [
-				'value6_1_1_1', 'value6_1_1_2'
+	key6: {
+		key61: {
+			key611: [
+				'value6111', 'value6112'
 			]
 		}
 	},
-	'key7': [
-		['value7_1', {'key7_2': 'value7_2'}]
+	key7: [
+		['value71', {key72: 'value72'}]
 	],
-	'key8': [
-		[{'key8_1': ['value_8_1', 'value_8_2']}]
+	key8: [
+		[{key81: ['value81', 'value82']}]
 	],
-	'key9': [
-		'key9_1'
+	key9: [
+		'key91'
 	],
-	'key10': {
-		'key10_1': [
-			'value10_1_1', 'value10_1_2', {'key10_1_3': 'value10_1_3'}
+	key10: {
+		key101: [
+			'value1011', 'value1012', {key1013: 'value1013'}
 		]
-	},
-
+	}
 };
 
 const expected = [
 	'key1="value1"',
 	'key2=1',
 	'key3=[3,"value3"]',
-	'key4={"key4_1":"value4_1","key4_2":"value4_2"}',
-	'key5=["value5_1","value5_1","value5_1"]',
-	'key6={"key6_1":{"key6_1_1":["value6_1_1_1","value6_1_1_2"]}}',
-	'key7=[["value7_1",{"key7_2":"value7_2"}]]',
-	'key8=[[{"key8_1":["value_8_1","value_8_2"]}]]',
-	'key9=["key9_1"]',
-	'key10={"key10_1":["value10_1_1","value10_1_2",{"key10_1_3":"value10_1_3"}]}'
+	'key4={"key41":"value41","key42":"value42"}',
+	'key5=["value51","value51","value51"]',
+	'key6={"key61":{"key611":["value6111","value6112"]}}',
+	'key7=[["value71",{"key72":"value72"}]]',
+	'key8=[[{"key81":["value81","value82"]}]]',
+	'key9=["key91"]',
+	'key10={"key101":["value1011","value1012",{"key1013":"value1013"}]}'
 ];
-
 
 test('returns well-serialized string', t => {
 	const values = fn.pairify(target);
@@ -57,14 +55,10 @@ test('returns well-serialized string', t => {
 	expected.forEach(function (v, i) {
 		t.is(values[i], v);
 	});
-
-	t.end();
 });
 
 test('deserialized strings', t => {
 	for (const [key, v] of Object.entries(fn.parse(fn.pairify(target)))) {
-		t.ok(deep(v, target[key]));
+		t.true(deep(v, target[key]));
 	}
-
-	t.end();
 });
